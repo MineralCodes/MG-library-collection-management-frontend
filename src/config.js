4f70f89b151851e1ded9caf48077e8be2ced966e
Micah Gilbertson <micah.gilbertson@gmail.com>
2020-12-10 16:39:31 -0600

    forgot to switch api back to heroku server

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const apiUrl2 = "https://library-collection-api.herokuapp.com";
-export const apiUrl = "http://127.0.0.1:5000";
+export const apiUrl = "https://library-collection-api.herokuapp.com";
+export const apiUrl2 = "http://127.0.0.1:5000";
